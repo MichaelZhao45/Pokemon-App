27c4eab74ced7f63bd47245d79b957ba6953f907
Michael Zhao <mzhao347@uwo.ca>
2024-07-09 22:04:54 -0500

    Finished getStats function and working on getDescription function

--- script.js
+++ script.js
@@ -1,3 +1,12 @@
+// sanitize HTML to prevent XSS attacks
+/*
+var sanitizeHTML = function (str) {
+	var temp = document.createElement('div');
+	temp.textContent = str;
+	return temp.innerHTML;
+};
+*/
+
 function getImage(data) {           // get pokemon sprite image
 
     const pokemonSprite = data.sprites.front_default;
@@ -9,13 +18,48 @@ function getImage(data) {           // get pokemon sprite image
 
 }
 
-function getStats() {           // base stats
+function getStats(data) {           // base stats
+
+    const pokemonStats = data.stats;
+    const statIds = ["hp", "attack", "defense", "specialAttack", "specialDefence", "speed"];
+
+    for (let i = 0; i < pokemonStats.length; i++) {             // pokemonStats[i].base_stat === object, base_stat is the actual instance var
+        let statElement = document.getElementById(statIds[i]);
+        statElement.innerHTML = statIds[i] + pokemonStats[i].base_stat;//sanitizeHTML(statIds[i]) + sanitizeHTML(pokemonStats[i].base_stat);
+    }   
+
 
 }
 
-function getDescription() {     // get weight, height, type, abilities, and pokedex id
+function getDescription(data) {     // get weight, height, type, abilities, and pokedex id
+
+    // get description elements that aren't an array
+    const id = data.id;
+    const idElement = document.getElementById('id');
+    idElement.innerHTML = "ID: " + id;//sanitizeHTML(id);
+
+    const weight = data.weight;
+    const weightElement = document.getElementById('weight');
+    weightElement.innerHTML = "Weight: " + weight;//sanitizeHTML(weight);
+
+    const height = data.height;
+    const heightElement = document.getElementById('height');
+    heightElement.innerHTML = "Height: " + height;//sanitizeHTML(height); 
+    
+
+
+
 
 }
+
+function convertHeight() {              // height: decimetres -> metres
+
+}
+
+function convertWeight() {              // weight: hectograms -> kg
+
+}
+
 
 async function main() {         // async main function for script
 
@@ -33,6 +77,8 @@ async function main() {         // async main function for script
         console.log(data);
 
         getImage(data);
+        getStats(data);
+        getDescription(data);
 
     }
     catch(error) {
